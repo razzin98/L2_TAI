fc938377f43f926be1187f6860dac5f663454751
razzin <mastertogo@vp.pl>
2021-03-19 16:21:31 +0100

    navbar_component

---
+++ navbar.js
@@ -0,0 +1,5 @@
+let nav='<nav><a href="index.html">Home</nav>' +
+    '<a href="contact.html">Kontakt</a> </nav>';
+
+let navbarElem = document.querySelector(".navbar"); //lub inny sposób opisany wyżej
+navbarElem.innerHTML = nav;
